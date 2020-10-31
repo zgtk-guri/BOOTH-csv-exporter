95ed8e3525e9db3d9a0cd84d7b917bde6ccdeed7
zgtk-guri <11141699+zgtk-guri@users.noreply.github.com>
2020-11-01 00:53:28 +0900

    Add project

---
+++ main.py
@@ -0,0 +1,53 @@
+import sys
+import csv
+import re
+from bs4 import BeautifulSoup
+
+def main(args):
+    if len(args) < 3:
+        print(f'Usage: {args[0]} InputHTMLFilePath OutputCsvPath')
+        exit(1)
+
+    input_file_path = args[1]
+    output_file_path = args[2]
+
+    with open(output_file_path, 'w', encoding='shift_jis') as csv_output, \
+            open(input_file_path, 'r', encoding='utf-8', newline='') as html_input:
+        soup = BeautifulSoup(html_input, 'html.parser')
+        orders_soup = soup.find_all('div', {'class': 'accordion-body'})
+
+        output_list = list()
+
+        for order_soup in orders_soup:
+            order_no = ''
+            order_date = ''
+            income_amount = 0
+            outgo_records = dict()
+            for row in order_soup.children:
+                name: str = row.contents[0].get_text()
+                value: str = row.contents[1].get_text()
+                negative: bool = 'co-breakdown-table-value-negative' in row.contents[1]['class']
+
+                if name.startswith('注文番号'):
+                    order_no = value
+                elif name.startswith('注文日時'):
+                    order_date = value
+                else:
+                    v = int(value.translate(str.maketrans('', '', ', ¥')))
+                    if negative:
+                        outgo_records[name] = v
+                    else:
+                        income_amount += v
+
+            order_date = re.match(r'(\d*)年(\d*)月(\d*)日 \d*時\d*分', order_date).expand(r'\1-\2-\3')
+
+            output_list.append(['収入', order_date, '売上高', income_amount, 'BOOTH', 'BOOTH', order_no])
+            for name, outgo in outgo_records.items():
+                output_list.append(['', order_date, '支払手数料' if name.startswith('手数料') else '荷造運賃', -outgo, 'BOOTH', '', ''])
+
+        csv_writer = csv.writer(csv_output)
+        csv_writer.writerows(output_list)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
